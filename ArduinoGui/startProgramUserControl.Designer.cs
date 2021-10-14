5e39841de1a3c39fbe185acf334b2219b37b09a4
mariopaja <mpaja15@epika.edu.al>
2021-10-14 11:14:45 +0200

    start program graphical changes

--- ArduinoGui/startProgramUserControl.Designer.cs
+++ ArduinoGui/startProgramUserControl.Designer.cs
@@ -54,16 +54,16 @@ namespace ArduinoGui
             this.startTimePicker.Font = new System.Drawing.Font("Arial", 9F);
             this.startTimePicker.Location = new System.Drawing.Point(128, 36);
             this.startTimePicker.Name = "startTimePicker";
-            this.startTimePicker.Size = new System.Drawing.Size(200, 28);
+            this.startTimePicker.Size = new System.Drawing.Size(200, 21);
             this.startTimePicker.TabIndex = 0;
             // 
             // startTimeLabel
             // 
             this.startTimeLabel.AutoSize = true;
             this.startTimeLabel.Font = new System.Drawing.Font("Arial", 9F);
-            this.startTimeLabel.Location = new System.Drawing.Point(25, 41);
+            this.startTimeLabel.Location = new System.Drawing.Point(26, 39);
             this.startTimeLabel.Name = "startTimeLabel";
-            this.startTimeLabel.Size = new System.Drawing.Size(121, 21);
+            this.startTimeLabel.Size = new System.Drawing.Size(83, 15);
             this.startTimeLabel.TabIndex = 38;
             this.startTimeLabel.Text = "Starting Time:";
             // 
@@ -84,7 +84,7 @@ namespace ArduinoGui
             this.lightProgramButton.Font = new System.Drawing.Font("Arial", 9F);
             this.lightProgramButton.Location = new System.Drawing.Point(128, 74);
             this.lightProgramButton.Name = "lightProgramButton";
-            this.lightProgramButton.Size = new System.Drawing.Size(197, 25);
+            this.lightProgramButton.Size = new System.Drawing.Size(140, 19);
             this.lightProgramButton.TabIndex = 40;
             this.lightProgramButton.TabStop = true;
             this.lightProgramButton.Text = "Illuminance Program";
@@ -96,7 +96,7 @@ namespace ArduinoGui
             this.soilTemperatureProgramButton.Font = new System.Drawing.Font("Arial", 9F);
             this.soilTemperatureProgramButton.Location = new System.Drawing.Point(128, 101);
             this.soilTemperatureProgramButton.Name = "soilTemperatureProgramButton";
-            this.soilTemperatureProgramButton.Size = new System.Drawing.Size(244, 25);
+            this.soilTemperatureProgramButton.Size = new System.Drawing.Size(170, 19);
             this.soilTemperatureProgramButton.TabIndex = 41;
             this.soilTemperatureProgramButton.TabStop = true;
             this.soilTemperatureProgramButton.Text = "Soil Temperature Program";
@@ -106,9 +106,9 @@ namespace ArduinoGui
             // 
             this.selectAutomatedProgramLabel.AutoSize = true;
             this.selectAutomatedProgramLabel.Font = new System.Drawing.Font("Arial", 9F);
-            this.selectAutomatedProgramLabel.Location = new System.Drawing.Point(27, 76);
+            this.selectAutomatedProgramLabel.Location = new System.Drawing.Point(26, 75);
             this.selectAutomatedProgramLabel.Name = "selectAutomatedProgramLabel";
-            this.selectAutomatedProgramLabel.Size = new System.Drawing.Size(138, 21);
+            this.selectAutomatedProgramLabel.Size = new System.Drawing.Size(95, 15);
             this.selectAutomatedProgramLabel.TabIndex = 42;
             this.selectAutomatedProgramLabel.Text = "Select Program:";
             // 
@@ -118,7 +118,7 @@ namespace ArduinoGui
             this.soilMoistureProgramButton.Font = new System.Drawing.Font("Arial", 9F);
             this.soilMoistureProgramButton.Location = new System.Drawing.Point(128, 126);
             this.soilMoistureProgramButton.Name = "soilMoistureProgramButton";
-            this.soilMoistureProgramButton.Size = new System.Drawing.Size(212, 25);
+            this.soilMoistureProgramButton.Size = new System.Drawing.Size(147, 19);
             this.soilMoistureProgramButton.TabIndex = 43;
             this.soilMoistureProgramButton.TabStop = true;
             this.soilMoistureProgramButton.Text = "Soil Moisture Program";
@@ -130,7 +130,7 @@ namespace ArduinoGui
             this.airHumidityProgramButton.Font = new System.Drawing.Font("Arial", 9F);
             this.airHumidityProgramButton.Location = new System.Drawing.Point(128, 151);
             this.airHumidityProgramButton.Name = "airHumidityProgramButton";
-            this.airHumidityProgramButton.Size = new System.Drawing.Size(203, 25);
+            this.airHumidityProgramButton.Size = new System.Drawing.Size(141, 19);
             this.airHumidityProgramButton.TabIndex = 44;
             this.airHumidityProgramButton.TabStop = true;
             this.airHumidityProgramButton.Text = "Air Humidity Program";
@@ -145,9 +145,10 @@ namespace ArduinoGui
             this.automatedProgramGroupBox.Controls.Add(this.startTimeLabel);
             this.automatedProgramGroupBox.Controls.Add(this.selectAutomatedProgramLabel);
             this.automatedProgramGroupBox.Controls.Add(this.soilTemperatureProgramButton);
+            this.automatedProgramGroupBox.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.automatedProgramGroupBox.Location = new System.Drawing.Point(17, 64);
             this.automatedProgramGroupBox.Name = "automatedProgramGroupBox";
-            this.automatedProgramGroupBox.Size = new System.Drawing.Size(441, 209);
+            this.automatedProgramGroupBox.Size = new System.Drawing.Size(460, 210);
             this.automatedProgramGroupBox.TabIndex = 45;
             this.automatedProgramGroupBox.TabStop = false;
             this.automatedProgramGroupBox.Text = "Automated Program";
@@ -156,9 +157,10 @@ namespace ArduinoGui
             // automatedProgramRadioButton
             // 
             this.automatedProgramRadioButton.AutoSize = true;
+            this.automatedProgramRadioButton.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.automatedProgramRadioButton.Location = new System.Drawing.Point(93, 24);
             this.automatedProgramRadioButton.Name = "automatedProgramRadioButton";
-            this.automatedProgramRadioButton.Size = new System.Drawing.Size(177, 24);
+            this.automatedProgramRadioButton.Size = new System.Drawing.Size(135, 19);
             this.automatedProgramRadioButton.TabIndex = 46;
             this.automatedProgramRadioButton.TabStop = true;
             this.automatedProgramRadioButton.Text = "Automated Program";
@@ -168,9 +170,10 @@ namespace ArduinoGui
             // manualProgramRadioButton
             // 
             this.manualProgramRadioButton.AutoSize = true;
+            this.manualProgramRadioButton.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.manualProgramRadioButton.Location = new System.Drawing.Point(289, 24);
             this.manualProgramRadioButton.Name = "manualProgramRadioButton";
-            this.manualProgramRadioButton.Size = new System.Drawing.Size(150, 24);
+            this.manualProgramRadioButton.Size = new System.Drawing.Size(116, 19);
             this.manualProgramRadioButton.TabIndex = 47;
             this.manualProgramRadioButton.TabStop = true;
             this.manualProgramRadioButton.Text = "Manual Program";
@@ -181,9 +184,10 @@ namespace ArduinoGui
             // 
             this.manualProgramGroupBox.Controls.Add(this.label1);
             this.manualProgramGroupBox.Controls.Add(this.txtFileAddressSelector);
+            this.manualProgramGroupBox.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.manualProgramGroupBox.Location = new System.Drawing.Point(17, 64);
             this.manualProgramGroupBox.Name = "manualProgramGroupBox";
-            this.manualProgramGroupBox.Size = new System.Drawing.Size(441, 209);
+            this.manualProgramGroupBox.Size = new System.Drawing.Size(460, 210);
             this.manualProgramGroupBox.TabIndex = 48;
             this.manualProgramGroupBox.TabStop = false;
             this.manualProgramGroupBox.Text = "Manual Program";
@@ -192,29 +196,30 @@ namespace ArduinoGui
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(6, 41);
+            this.label1.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(11, 43);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(151, 20);
+            this.label1.Size = new System.Drawing.Size(118, 15);
             this.label1.TabIndex = 1;
             this.label1.Text = "Select Program File:";
             // 
             // txtFileAddressSelector
             // 
             this.txtFileAddressSelector.Cursor = System.Windows.Forms.Cursors.Arrow;
-            this.txtFileAddressSelector.Location = new System.Drawing.Point(164, 40);
+            this.txtFileAddressSelector.Location = new System.Drawing.Point(131, 40);
             this.txtFileAddressSelector.Name = "txtFileAddressSelector";
             this.txtFileAddressSelector.ReadOnly = true;
-            this.txtFileAddressSelector.Size = new System.Drawing.Size(258, 26);
+            this.txtFileAddressSelector.Size = new System.Drawing.Size(310, 21);
             this.txtFileAddressSelector.TabIndex = 0;
             this.txtFileAddressSelector.Click += new System.EventHandler(this.txtFileAddressSelector_Click);
             // 
             // startProgramUserControl
             // 
-            this.Controls.Add(this.manualProgramGroupBox);
             this.Controls.Add(this.manualProgramRadioButton);
             this.Controls.Add(this.automatedProgramRadioButton);
-            this.Controls.Add(this.automatedProgramGroupBox);
             this.Controls.Add(this.transferDataButton);
+            this.Controls.Add(this.automatedProgramGroupBox);
+            this.Controls.Add(this.manualProgramGroupBox);
             this.Name = "startProgramUserControl";
             this.Size = new System.Drawing.Size(679, 455);
             this.automatedProgramGroupBox.ResumeLayout(false);
